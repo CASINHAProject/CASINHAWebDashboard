4796ec79a8b9327eb364fb8e93070889537d92bc
diegofsousa <diegofernando5672@gmail.com>
2017-06-07 01:44:38 -0300

    add House's app

---
+++ house/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.utils import timezone
+
+from core.models import User
+
+class House(models.Model):
+	name = models.CharField(max_length=100, verbose_name='Nome', null=False, blank=False)
+	creator = models.ForeignKey(User, verbose_name='Criador', null=False, blank=False, related_name='+')
+	crated_by = models.DateTimeField(default=timezone.now)
+	server = models.CharField(max_length=100, verbose_name='Servidor', null=False, blank=False)
+	user = models.CharField(max_length=100, verbose_name='Usuário', null=False, blank=False)
+	password = models.CharField(max_length=100, verbose_name='Senha', null=False, blank=False)
+	portws = models.CharField(max_length=100, verbose_name='Porta para websocket', null=False, blank=False)
+	participants = models.ManyToManyField(User, verbose_name='Participantes')
+
+	def __str__(self):
+		return self.name
