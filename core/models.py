d79edd770b4f0fadd939427e2af71a946718c2fd
diegofsousa <diegofernando5672@gmail.com>
2017-06-13 18:33:52 -0300

    aux_tags started

--- core/models.py
+++ core/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import Permission, User
 
+class ImageData(models.Model):
+	user = models.ForeignKey(User, null=False, blank=False, related_name='+')
+	profile = models.FileField(upload_to='profile/', null=True, blank=True)
+	cover = models.FileField(upload_to='cover/', null=True, blank=True)
+	
+	def __str__(self):
+		return 'Dados de ' + self.user.username
